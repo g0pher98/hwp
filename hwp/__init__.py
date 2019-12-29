61fce1cd2ad6dfc7a1d9ae9915a6c917bcf4e3ee
g0pher-git <g0pher@tistory.com>
2019-12-29 22:16:36 +0900

    dev header parser func

--- hwp/__init__.py
+++ hwp/__init__.py
@@ -5,3 +5,22 @@ def print_hello():
 def open_hwp():
 	print("hwp is opened")
 	return True
+
+
+
+'''
+
+>>> f = HWP("./test.hwp")
+>>> f.info()
+{
+	'version' : 'hwp v5',
+	'size' : 1736,
+	'hash' : {
+		'md5' : '~~~',
+		'sha1' : '~~~'
+	}
+}
+
+
+
+'''
---
+++ hwp/parser/__init__.py
@@ -0,0 +1,42 @@
+def load():
+	f = open("test.hwp",'rb')
+	return f.read()
+
+def header(raw):
+	'''
+	[INFO]
+	@author : Lee Jae Seung
+	
+	[DESCRIPTION]
+	OLE 헤더를 딕셔너리화 하는 함수. 각 키와 설명은 다음과 같다.
+	
+	[MORE]
+	signature : OLE 파일 시그니처
+	number_of_BBAT_Depot : BBAT(Big Block Allocation Table)의 Depot(보관소)의 개수
+	number_of_SBAT_Depot : SBAT(Small Block Allocation Table)의 Depot의 개수
+	property_start : 스토리지와 스트림에 대한 정보가 담긴 블록 번호
+	SBAT_start : SBAT 시작블록 번호
+	BBAT_array : BBAT의 블록 정보가 담긴 배열(각 4bytes)
+	'''
+	data = {}
+	data['signature'] = raw[0:8]
+	data['number_of_BBAT_Depot'] = int.from_bytes(raw[44:48], "little")
+	data['number_of_SBAT_Depot'] = int.from_bytes(raw[64:68], "little")
+	data['property_start'] = int.from_bytes(raw[48:52], "little")
+	data['SBAT_start'] = int.from_bytes(raw[60:64], "little")
+	data['BBAT_array'] = []
+	bbat_array_length = data['number_of_BBAT_Depot']*4
+	bbat_array = raw[76:76 + bbat_array_length]
+	for idx in range(0, len(bbat_array), 4):
+		data['BBAT_array'].append(int.from_bytes(bbat_array[idx:idx + 4], "little"))
+	return data
+
+
+def get_block(raw, number):
+	'''
+	@author : Lee Jae Seung
+	
+	특정 번호에 해당하는 블록 데이터를 리턴하는 함수.
+	'''
+	idx = (number+1) * 512
+	return raw[idx:idx + 512]
