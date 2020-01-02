148c9db04fda85cc3cf1ebd6ded06df056a7262a
g0pher-git <g0pher@tistory.com>
2020-01-02 14:17:18 +0900

    mod

--- hwp/parser/__init__.py
+++ hwp/parser/__init__.py
@@ -88,7 +88,7 @@ def fileheader(raw):
 	'''
 	@author : Lee Jae Seung
 	
-	FileHeader 스트림 파싱함수.
+	FileHeader(256Bytes) 스트림 파싱함수.
 	hwp 구조 5.0 문서 8page 참고하여 개발.
 	'''
 	data = {
@@ -131,3 +131,70 @@ def fileheader(raw):
 		'reservation' :
 	}
 	return data
+
+def docinfo(raw):
+	'''
+	@author : Lee Jae Seung
+	'''
+	data = {
+		'HWPTAG_DOCUMENT_PROPERTIES' : 30,
+		'HWPTAG_ID_MAPPINGS' : 32,
+		'HWPTAG_BIN_DATA' : 가변,
+		'HWPTAG_FACE_NAME' : 가변,
+		'HWPTAG_BORDER_FILL' : 가변,
+		'HWPTAG_CHAR_SHAPE' : 72,
+		'HWPTAG_TAB_DEF' : 14,
+		'HWPTAG_NUMBERING' : 가변,
+		'HWPTAG_BULLET' : 10,
+		'HWPTAG_PARA_SHAPE' : 54,
+		'HWPTAG_STYLE' : 가변,
+		'HWPTAG_MEMO_SHAPE' : 22,
+		'HWPTAG_TRACK_CHANGE_AUTHOR' : 가변,
+		'HWPTAG_TRACK_CHANGE' : 가변,
+		'HWPTAG_DOC_DATA' : 가변,
+		'HWPTAG_FORBIDDEN_CHAR' : 가변,
+		'HWPTAG_COMPATIBLE_DOCUMENT' : 4,
+		'HWPTAG_LAYOUT_COMPATIBILITY' : 20,
+		'HWPTAG_DISTRIBUTE_DOC_DATA' : 256,
+		'HWPTAG_TRACKCHANGE' : 1032
+	}
+	return data
+
+def bodytext_section(raw):
+	'''
+	@author : Lee Jae Seung
+	'''
+	data = {
+		'HWPTAG_PARA_HEADER' : 22,
+		'HWPTAG_PARA_TEXT' : 가변,
+		'HWPTAG_PARA_CHAR_SHAPE' : 가변,
+		'HWPTAG_PARA_LINE_SEG' : 가변,
+		'HWPTAG_PARA_RANGE_TAG' : 가변,
+		'HWPTAG_CTRL_HEADER' : 4 ,
+		'HWPTAG_LIST_HEADER' : 6 ,
+		'HWPTAG_PAGE_DEF' : 40,
+		'HWPTAG_FOOTNOTE_SHAPE' : 30,
+		'HWPTAG_PAGE_BORDER_FILL' : 14,
+		'HWPTAG_SHAPE_COMPONENT' : 4 ,
+		'HWPTAG_TABLE' : 가변,
+		'HWPTAG_SHAPE_COMPONENT_LINE' : 20,
+		'HWPTAG_SHAPE_COMPONENT_RECTANGLE' : 9 ,
+		'HWPTAG_SHAPE_COMPONENT_ELLIPSE' : 60,
+		'HWPTAG_SHAPE_COMPONENT_ARC' : 25,
+		'HWPTAG_SHAPE_COMPONENT_POLYGON' : 가변,
+		'HWPTAG_SHAPE_COMPONENT_CURVE' : 가변,
+		'HWPTAG_SHAPE_COMPONENT_OLE' : 26,
+		'HWPTAG_SHAPE_COMPONENT_PICTURE' : 가변,
+		'HWPTAG_CTRL_DATA' : 가변,
+		'HWPTAG_EQEDIT' : 가변,
+		'HWPTAG_SHAPE_COMPONENT_TEXTART' : 가변,
+		'HWPTAG_FORM_OBJECT' : 가변,
+		'HWPTAG_MEMO_SHAPE' : 22,
+		'HWPTAG_MEMO_LIST' : 4 ,
+		'HWPTAG_CHART_DATA' : 2 ,
+		'HWPTAG_VIDEO_DATA' : 가변,
+		'HWPTAG_SHAPE_COMPONENT_UNKNOWN' : 36
+	}
+	return data
+
+
